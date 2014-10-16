2582fd22210f8dbe1ab33ff443a0be40e169cba0
Peter Jones <pjones@redhat.com>
2014-10-16 16:18:05 -0400

    Make test.sh try to do some action.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONPATH=$PWD exec ./tools/bootcfgmgr --config-file=$PWD/data/grub2-efi.cfg
+PYTHONPATH=$PWD exec ./tools/bootcfgmgr --config-file=$PWD/data/grub2-efi.cfg --default-kernel "$@"
