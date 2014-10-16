fcfc6d4c812d3f4be5cf324eaee5fc62f2b56afc
Peter Jones <pjones@redhat.com>
2014-10-16 15:05:41 -0400

    Just some housekeeping.
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+    name = "bootmgrcfg",
+    packages = ["bootmgrcfg"],
+    version = "0.1",
+    description = "indescribable",
+    author = "Peter Jones",
+    author_email = "pjones@redhat.com",
+    url = "https://github.com/vathpela/bootcfgmgr",
+)
+
