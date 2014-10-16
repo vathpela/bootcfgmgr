7673eb8bcfd6331b5b04342d90ed13d3af7c4bd4
Peter Jones <pjones@redhat.com>
2014-10-16 16:17:44 -0400

    Make grub2 config files able to have efi forced.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- bootcfgmgr/loaders/grub2.py
+++ bootcfgmgr/loaders/grub2.py
@@ -10,12 +10,29 @@ class Grub2(Loader):
     _name = None
     _supported = True
     _arches = [ "x86_64", "i386", "i686", "aarch64", "ppc64", "ppc64le" ]
-    _config_files = [
+    _all_config_files = [
             "/etc/grub2-efi.cfg",
             "/etc/grub2.cfg",
             "/boot/grub2/grub.cfg",
             "/boot/grub2-efi/grub.cfg",
             "/boot/grub/grub.cfg",
         ]
+    _efi_config_files = [
+            "/etc/grub2-efi.cfg",
+            "/boot/grub2-efi/grub.cfg",
+        ]
+
+    def __init__(self, **kwargs):
+        Loader.__init__(self, **kwargs)
+        self.forceEfi = False
+
+    @property
+    def _config_files(self):
+        for x in self._all_config_files:
+            if self.forceEfi:
+                if x in self._efi_config_files:
+                    yield x
+            else:
+                yield x
 
 register_loader(Grub2)
